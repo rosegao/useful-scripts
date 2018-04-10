a56c4375be3938c8cbf532b177ed4e49e1372d7c
Rose Gao <rose.gao6@gmail.com>
2018-04-09 19:47:27 -0500

    added certs for jupyter notebook

--- gcp_setup_script.sh
+++ gcp_setup_script.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-# Update and install bzip2
+### Update and install bzip2
 sudo apt-get update
 sudo apt-get --yes upgrade
 sudo apt-get install bzip2
 
-# Install Anaconda
+### Install Anaconda
 mkdir downloads
 cd downloads
 wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
 bash Anaconda3-4.3.0-Linux-x86_64.sh -b
-
-
 echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
 export PATH=~/anaconda3/bin:$PATH
 conda install -y bcolz
 conda upgrade -y --all
 
-# Config Jupyter notebook
-mkdir notebooks
-cd notebooks
+### Start Jupyter notebook
+# create config file
 jupyter notebook --generate-config
+
+# create certs for https
+mkdir certs
+cd certs
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out mycert.pem
+
+cd ~
+echo "c.NotebookApp.certfile = u'~/certs/mycert.pem'" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.ip = '*'" >> $HOME/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.open_browser = False" >> $HOME/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.port = 8888" >> $HOME/.jupyter/jupyter_notebook_config.py
